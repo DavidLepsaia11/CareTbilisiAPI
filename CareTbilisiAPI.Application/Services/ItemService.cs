d2a849d98a16a23664077a8f669fc09f2cbc1077
dlepsaia <dlepsaia@DEV013>
2023-06-19 14:26:55 +0400

    Added ItemService

--- CareTbilisiAPI.Application/Services/ItemService.cs
+++ CareTbilisiAPI.Application/Services/ItemService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CareTbilisiAPI.Domain.Interfaces.Repositories;
+using CareTbilisiAPI.Domain.Interfaces.Services;
+using CareTbilisiAPI.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,11 @@ using System.Threading.Tasks;
 
 namespace CareTbilisiAPI.Application.Services
 {
-    public class ItemService
+    public class ItemService : BaseService<Item, IItemRepository>
     {
+        public ItemService(IItemRepository repository) : base(repository)
+        {
 
+        }
     }
 }
